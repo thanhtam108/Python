1510c6314e07131552ff9df8c9cb9399aeca15cb
thanhtam108 <jamewalker0203@gmail.com>
2023-12-15 21:40:36 +0700

    ok

---
+++ week3.py
@@ -0,0 +1,26 @@
+# 1. Create a class representing a bank account. Include attributes like account number, account balance, 
+#     and account type. Implement methods to deposit and withdraw money from the account.
+
+class bankAccount:
+    
+    def __init__ (self, accountNumber, accountBalance, accountType):
+        self.accountNumber = accountNumber
+        self.accountBalance = accountBalance
+        self.accountType = accountType
+    
+
+# 2.  Create a class representing a book. Include attributes like title, author, genre, and publication date. 
+#     Implement methods to lend and return the book.
+# 3.  Create a class representing a car. Include attributes like make, model, year, and color. 
+#     Implement methods to start and stop the car, accelerate, and brake.
+# 4.  Create a class representing a student management system. Include attributes like student name, student ID, 
+#     and student grades. Implement methods to add and remove students, and to calculate student averages.
+# 5.  Create a class representing a social media platform. Include attributes like user name, user profile, 
+#     and user posts. Implement methods to create and delete user accounts, and to post and share content.
+# 6. Create a class representing a deck of cards. Implement methods to shuffle the deck, draw a card, 
+#     and add a card back to the deck.
+# 7. Create a class representing a binary tree. Implement methods to insert, search, and delete nodes from the tree.
+# 8. Create a class representing a linked list. Implement methods to insert, search, and delete nodes from the list.
+# 9. Create a class representing a hash table. Implement methods to put, get, and 
+#     remove key-value pairs from the hash table.
+# 10. Create a class representing a web server. Implement methods to handle HTTP requests and to serve web pages.
